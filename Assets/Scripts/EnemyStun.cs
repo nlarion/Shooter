15661f54bcae09d6cf203cf0f4448772d3a57f4b
Neil Larion <neil.larion@gmail.com>
2016-05-17 16:13:35 -0700

    Add ability for player to shoot both forward and backward depending on which way they're facing

--- Assets/Scripts/EnemyStun.cs
+++ Assets/Scripts/EnemyStun.cs
@@ -6,13 +6,19 @@ public class EnemyStun : MonoBehaviour {
     // if Player hits the stun point of the enemy, then call Stunned on the enemy
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Player" || other.gameObject.tag =="Projectile")
+        if (other.gameObject.tag == "Player" )
         {
             // tell the enemy to be stunned
             this.GetComponentInParent<Enemy>().Stunned();
 
             //make player bounce off enemy
             other.gameObject.GetComponent<CharacterController2D>().EnemyBounce();
+        }
+        else if (other.gameObject.tag == "Projectile")
+        {
+            // tell the enemy to be stunned
+            this.GetComponentInParent<Enemy>().Stunned();
+
         }
     }
 }
